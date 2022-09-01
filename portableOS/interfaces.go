ab4cbf94bf8bd901bfbb2c45458bbe6ab456d581
Sydney Anne Erickson <sydney@elixxir.io>
2022-09-01 15:44:37 -0700

    Update copyright headers

--- portableOS/interfaces.go
+++ portableOS/interfaces.go
@@ -1,9 +1,9 @@
-///////////////////////////////////////////////////////////////////////////////
-// Copyright © 2020 xx network SEZC                                          //
-//                                                                           //
-// Use of this source code is governed by a license that can be found in the //
-// LICENSE file                                                              //
-///////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+// Copyright © 2022 xx foundation                                             //
+//                                                                            //
+// Use of this source code is governed by a license that can be found in the  //
+// LICENSE file.                                                              //
+////////////////////////////////////////////////////////////////////////////////
 
 // Package portableOS contains global OS functions that can be overwritten to be
 // used with other filesystems not supported by the [os] package, such as wasm.
