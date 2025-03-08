1ff63bba01e882ecef2d393fce6be64a86520304
thisisommore <ommore501@gmail.com>
2025-03-08 10:48:38 +0000

    replace LocalStorage with ExternalStorage

--- portableOS/wasm_js.go
+++ portableOS/wasm_js.go
@@ -15,35 +15,38 @@ import (
 	"gitlab.com/elixxir/wasm-utils/storage"
 )
 
-// Wrapper for Javascript localStorage.
-var localStorage = storage.GetLocalStorage()
+// Wrapper for Javascript externalStorage.
+var externalStorage = storage.GetExternalStorage()
 
 // Open opens the named file for reading. If successful, methods on the returned
 // file can be used for reading.
 var Open = func(name string) (File, error) {
-	keyValue, err := localStorage.Get(name)
+	keyValue, err := externalStorage.Get(name)
 	if err != nil {
 		return nil, err
 	}
 
-	return open(name, string(keyValue), localStorage), nil
+	return open(name, string(keyValue), externalStorage), nil
 }
 
 // Create creates or truncates the named file. If the file already exists, it is
 // truncated. If the file does not exist, it is created. If successful, methods
 // on the returned File can be used for I/O.
 var Create = func(name string) (File, error) {
-	err := localStorage.Set(name, []byte(""))
+	err := externalStorage.Set(name, []byte(""))
 	if err != nil {
 		return nil, err
 	}
 
-	return open(name, "", localStorage), nil
+	return open(name, "", externalStorage), nil
 }
 
 // Remove removes the named file or directory.
 var Remove = func(name string) error {
-	localStorage.RemoveItem(name)
+	err := externalStorage.Delete(name)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -53,14 +56,16 @@ var Remove = func(name string) error {
 // returns nil (no error).
 // If there is an error, it will be of type *PathError.
 var RemoveAll = func(path string) error {
-	for i := 0; i < localStorage.Length(); i++ {
-		keyName, err := localStorage.Key(i)
-		if err != nil {
-			return err
-		}
-
+	keys, err := externalStorage.Keys()
+	if err != nil {
+		return err
+	}
+	for _, keyName := range keys {
 		if strings.HasPrefix(keyName, path) {
-			localStorage.RemoveItem(keyName)
+			err := externalStorage.Delete(keyName)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -72,17 +77,17 @@ var RemoveAll = func(path string) error {
 // umask) are used for all directories that MkdirAll creates. If path is already
 // a directory, MkdirAll does nothing and returns nil.
 var MkdirAll = func(path string, perm FileMode) error {
-	err := localStorage.Set(path, []byte(""))
+	err := externalStorage.Set(path, []byte(""))
 	if err != nil {
 		return err
 	}
-	open(path, "", localStorage)
+	open(path, "", externalStorage)
 	return nil
 }
 
 // Stat returns a FileInfo describing the named file.
 var Stat = func(name string) (FileInfo, error) {
-	keyValue, err := localStorage.Get(name)
+	keyValue, err := externalStorage.Get(name)
 	if err != nil {
 		return nil, err
 	}
